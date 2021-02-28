0a777986c09ef26a4599136d98066e4dfe3e77ce
KauanDK <lidiane6734@gmail.com>
2021-02-27 22:55:24 -0300

    First commit

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  NATZIN
+──────────────────
+        『 *VIP USER* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*AÍ SIM PAE, VOCÊ É UM MEMBRO VIP😎✋`
+}
+exports.cekvip = cekvip
